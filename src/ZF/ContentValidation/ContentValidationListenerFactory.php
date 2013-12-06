614e6929c524dd7c67606a99d86d7e5f5952a293
Matthew Weier O'Phinney <matthew@zend.com>
2013-12-06 09:05:22 -0600

    Created service factory for ContentValidationListener

---
+++ src/ZF/ContentValidation/ContentValidationListenerFactory.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\ContentValidation;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class ContentValidationListenerFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceLocatorInterface $services 
+     * @return ContentValidationListener
+     */
+    public function createService(ServiceLocatorInterface $services)
+    {
+        $config = [];
+        if ($services->has('Config')) {
+            $allConfig = $services->get('Config');
+            if (isset($allConfig['zf-content-validation'])) {
+                $config = $allConfig['zf-content-validation'];
+            }
+        }
+        return new ContentValidationListener($config, $services);
+    }
+}
