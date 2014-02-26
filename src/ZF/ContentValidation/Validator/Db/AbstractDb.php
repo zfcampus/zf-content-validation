ffcd85f92a02ceb8644b32eba71d9e49df66fcdd
Julien Guittard <julien.guittard@me.com>
2014-02-26 22:18:24 +0100

    Rollback in ctor with setOptions tweak

--- src/ZF/ContentValidation/Validator/Db/AbstractDb.php
+++ src/ZF/ContentValidation/Validator/Db/AbstractDb.php
@@ -3,7 +3,10 @@
 namespace ZF\ContentValidation\Validator\Db;
 
 use Zend\Validator\Db\AbstractDb as AbstractDbValidator;
+use Traversable;
 use Zend\Db\Adapter\Adapter as DbAdapter;
+use Zend\Db\Sql\Select;
+use Zend\Stdlib\ArrayUtils;
 use Zend\Validator\Exception;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
@@ -12,13 +15,85 @@ abstract class AbstractDb extends AbstractDbValidator implements ServiceLocatorA
 {
 	use ServiceLocatorAwareTrait;
 	
-	public function setAdapter($adapter)
+	public function __construct($options = null)
 	{
-		$adapterService = $this->getServiceLocator()->get($adapter);
-		if (!($adapterService instanceof DbAdapter)) {
-			throw new Exception\InvalidArgumentException('DbAdapter service not valid!');
+		if ($options instanceof Traversable) {
+			$options = ArrayUtils::iteratorToArray($options);
 		}
-		$this->adapter = $adapterService;
-		return $this;
+		
+		if (isset($this->messageTemplates)) {
+			$this->abstractOptions['messageTemplates'] = $this->messageTemplates;
+		}
+		
+		if (isset($this->messageVariables)) {
+			$this->abstractOptions['messageVariables'] = $this->messageVariables;
+		}
+		
+		if (is_array($options)) {
+			if (array_key_exists('adapter', $options)) {
+				$tempAdapter = $options['adapter'];
+				unset($options['adapter']);
+			}
+			$this->setOptions($options);
+			$options['adapter'] = $tempAdapter;
+		}
+		
+		if ($options instanceof Select) {
+			$this->setSelect($options);
+			return;
+		}
+	
+		if ($options instanceof Traversable) {
+			$options = ArrayUtils::iteratorToArray($options);
+		} elseif (func_num_args() > 1) {
+			$options       = func_get_args();
+			$firstArgument = array_shift($options);
+			if (is_array($firstArgument)) {
+				$temp = ArrayUtils::iteratorToArray($firstArgument);
+			} else {
+				$temp['table'] = $firstArgument;
+			}
+	
+			$temp['field'] = array_shift($options);
+	
+			if (!empty($options)) {
+				$temp['exclude'] = array_shift($options);
+			}
+	
+			if (!empty($options)) {
+				$temp['adapter'] = array_shift($options);
+			}
+	
+			$options = $temp;
+		}
+	
+		if (!array_key_exists('table', $options) && !array_key_exists('schema', $options)) {
+			throw new Exception\InvalidArgumentException('Table or Schema option missing!');
+		}
+	
+		if (!array_key_exists('field', $options)) {
+			throw new Exception\InvalidArgumentException('Field option missing!');
+		}
+	
+		if (array_key_exists('adapter', $options) && is_string($options['adapter'])) {
+			$adapter = $this->getServiceLocator()->get($options['adapter']);
+			if (!($adapter instanceof DbAdapter)) {
+				throw new Exception\InvalidArgumentException('DbAdapter service not valid!');
+			}
+			$this->setAdapter($adapter);
+		}
+	
+		if (array_key_exists('exclude', $options)) {
+			$this->setExclude($options['exclude']);
+		}
+	
+		$this->setField($options['field']);
+		if (array_key_exists('table', $options)) {
+			$this->setTable($options['table']);
+		}
+	
+		if (array_key_exists('schema', $options)) {
+			$this->setSchema($options['schema']);
+		}
 	}
 }
