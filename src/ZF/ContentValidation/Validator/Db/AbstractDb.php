0bee53fc4bead61f48acd896492440de49135686
Julien Guittard <julien.guittard@me.com>
2014-02-26 21:58:29 +0100

    Aliased AbstractDb class inheritance

--- src/ZF/ContentValidation/Validator/Db/AbstractDb.php
+++ src/ZF/ContentValidation/Validator/Db/AbstractDb.php
@@ -2,7 +2,7 @@
 
 namespace ZF\ContentValidation\Validator\Db;
 
-use Zend\Validator\Db\AbstractDb;
+use Zend\Validator\Db\AbstractDb as AbstractDbValidator;
 use Traversable;
 use Zend\Db\Adapter\Adapter as DbAdapter;
 use Zend\Db\Sql\Select;
@@ -11,7 +11,7 @@ use Zend\Validator\Exception;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
 
-abstract class AbstractDb extends AbstractDb implements ServiceLocatorAwareInterface
+abstract class AbstractDb extends AbstractDbValidator implements ServiceLocatorAwareInterface
 {
 	use ServiceLocatorAwareTrait;
 	
