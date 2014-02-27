b8c18f328d781cc74ecd9f1cdde9745bc49b5864
Matthew Weier O'Phinney <matthew@zend.com>
2014-02-27 16:04:20 -0600

    [#7] CS review
    - Remove [] array notation, in favor of array() (PHP 5.3 compat).
    - s/serviceLocator/validators/ as argument name (clarifies that these factories
      operate inside the ValidatorPluginManager).
    - Move arguments to multiple lines for readability.
    - Sort import statements.
    - Trailing whitespace, indentation.

--- src/ZF/ContentValidation/Validator/Db/RecordExistsFactory.php
+++ src/ZF/ContentValidation/Validator/Db/RecordExistsFactory.php
@@ -1,36 +1,47 @@
 <?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
+ */
 
 namespace ZF\ContentValidation\Validator\Db;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\MutableCreationOptionsInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
-use Zend\Validator\Db\RecordExists;
 use Zend\Stdlib\ArrayUtils;
+use Zend\Validator\Db\RecordExists;
 
-class RecordExistsFactory implements FactoryInterface, MutableCreationOptionsInterface 
+class RecordExistsFactory implements FactoryInterface, MutableCreationOptionsInterface
 {
-	/**
-	 * @var array
-	 */
-	protected $options = [];
-	
-	/**
-	 * Set options property
-	 *
-	 * @param array $options
-	 */
-	public function setCreationOptions(array $options)
-	{
-		$this->options = $options;
-	}
-	    
-    public function createService(ServiceLocatorInterface $serviceLocator) 
+    /**
+     * @var array
+     */
+    protected $options = array();
+
+    /**
+     * Set options property
+     *
+     * @param array $options
+     */
+    public function setCreationOptions(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @param ServiceLocatorInterface $validators
+     * @return RecordExists
+     */
+    public function createService(ServiceLocatorInterface $validators)
     {
         if (isset($this->options['adapter'])) {
-            return new RecordExists(ArrayUtils::merge($this->options, ['adapter' => $serviceLocator->getServiceLocator()->get($this->options['adapter'])]));
-        } else {
-            return new RecordExists($this->options);
+            return new RecordExists(ArrayUtils::merge(
+                $this->options,
+                array('adapter' => $validators->getServiceLocator()->get($this->options['adapter']))
+            ));
         }
+
+        return new RecordExists($this->options);
     }
 }
